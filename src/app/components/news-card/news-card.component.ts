38b8890814f3160ceed3601f7e26f91fb7cb5179
mohitsi2 <mohitsi2@cisco.com>
2022-05-15 01:48:46 +0530

    Update news card to dynamically support input data

--- src/app/components/news-card/news-card.component.ts
+++ src/app/components/news-card/news-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ArticlesEntity } from 'src/app/interfaces/news-response';
 
 @Component({
   selector: 'app-news-card',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-card.component.scss'],
 })
 export class NewsCardComponent implements OnInit {
+  @Input() news: ArticlesEntity;
   constructor() {}
 
   ngOnInit() {}
