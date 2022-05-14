b5229b5b71a41f302e6182faae538321a31323e4
mohitsi2 <mohitsi2@cisco.com>
2022-05-15 01:45:19 +0530

    Add baseURl and apiKey for NewsAPI

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseURL: 'https://newsapi.org/v2/top-headlines/',
+  apiKey: '3c5f524341494773876b6c2b35010346',
 };
 
 /*
